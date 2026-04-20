3d028f7457aa3410b9014fff8216fa1575d64ab0
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-04-20 15:28:05 +0300

    Remove tautological comments

--- axum-myroutes/tests/ui.rs
+++ axum-myroutes/tests/ui.rs
@@ -4,6 +4,6 @@
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/ui/pass_*.rs"); // должны компилироваться
-    t.compile_fail("tests/ui/fail_*.rs"); // должны НЕ компилироваться
+    t.pass("tests/ui/pass_*.rs");
+    t.compile_fail("tests/ui/fail_*.rs");
 }
